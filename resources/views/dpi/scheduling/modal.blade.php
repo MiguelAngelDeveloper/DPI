018ad41bd4380a141c031d8468ba8560e92a6eb9
MiguelAngelDeveloper <mascorreo2@hotmail.com>
2018-06-21 15:49:56 +0200

    Recolocación de campos en el modal para mejor aspecto

--- resources/views/dpi/scheduling/modal.blade.php
+++ resources/views/dpi/scheduling/modal.blade.php
@@ -19,15 +19,19 @@
               <dd>{{$window->duration}}</dd>
             </dl>
             <div class="form-group">
-              <label for="optimal_inertion_date" class="title mr-1">@lang('dpi.hoi')</label>
-              <input type=text name='optimal_insertion_date' id="optimal_inertion_date">
-              <input type=hidden name="windowId" value={{$window->id}}>
-              <label for="event_type" class="title mr-1">@lang('dpi.event_type')</label>
-              <select name="event_type" id="event_type">
-                <option value="LOI">LOI</option>
-                <option value="REM">REM</option>
-                <option value="NUL">NUL</option>
-              </select>
+              <div>
+                <label for="event_type" class="title mr-1">@lang('dpi.event_type')</label>
+                <select name="event_type" id="event_type">
+                  <option value="LOI">LOI</option>
+                  <option value="REM">REM</option>
+                  <option value="NUL">NUL</option>
+                </select>
+              </div>
+              <div>
+                <label for="optimal_inertion_date" class="title mr-1">@lang('dpi.hoi')</label>
+                <input type=text name='optimal_insertion_date' id="optimal_inertion_date">
+                <input type=hidden name="windowId" value={{$window->id}}>
+              </div>
             </div>
             <div class="form-group">
               <label for="spotSelect" class="title mr-1">@lang('dpi.ads')</label>

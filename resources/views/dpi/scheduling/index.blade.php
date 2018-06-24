9bfbdd71bfaeab3c8a661548379239729ee6145f
MiguelAngelDeveloper <37699288+MiguelAngelDeveloper@users.noreply.github.com>
2018-06-25 01:33:05 +0200

    Arreglo bug miga de pan

--- resources/views/dpi/scheduling/index.blade.php
+++ resources/views/dpi/scheduling/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
   <ol class="breadcrumb">
     <li class="breadcrumb-item">
-      <a href="{{ URL::to('scheling') }}">@lang('dpi.scheduling')</a>
+      <a href="{{ URL::to('scheduling') }}">@lang('dpi.scheduling')</a>
     </li>
     <li class="breadcrumb-item active">@lang('dpi.getSchedulings')</li>
   </ol>
